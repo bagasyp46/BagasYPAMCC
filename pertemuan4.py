967351c9ddb1087766d0a43b76611c7f7b48866d
bagasyp46 <bagas.3295@students.amikom.ac.id>
2019-12-21 15:17:07 +0700

    Pertemuan Desktop 4

---
+++ pertemuan4.py
@@ -0,0 +1,53 @@
+# dict = {'Andi Beli Mangga': '5 Kg', 'Jeruk': '10 Kg', 'Harga Mangga':'Rp.10.000', 'Harga Jeruk':' Rp. 1.000'}
+
+
+
+
+# print ("dict['Andi Beli Mangga']", dict['Andi Beli Mangga'],",['Jeruk']",dict['Jeruk'])
+# print ("dict['Harga Mangga']", dict['Harga Mangga'])
+# print ("dict['Harga Jeruk']", dict['Harga Jeruk'])
+
+
+
+# def luas_segitiga(alas,tinggi):
+#     luas = (alas*tinggi)/2
+#     print ('Luas Segitiga: %f'%luas);
+
+# luas_segitiga(4,6)
+
+
+# def Luas_Persegi(sisi):
+    
+#     return sisi*sisi
+
+
+# print ('Luas Persegi',Luas_Persegi(4))
+
+
+# def Luas_Segitiga(alas,tinggi):
+
+#     return ((alas*tinggi)/2)
+
+
+# print ('Luas Segitiga',Luas_Segitiga(6,8))
+
+
+nama = "Bagas"
+versi = "1.0.0"
+
+
+def help():
+
+    nama = "Programku"
+    versi = "1.0.1"
+
+    print ('Nama : %s '% nama)
+    print ('Versi : %s '% versi)
+
+
+print ('Nama : %s '% nama)
+print ('Versi : %s '% versi)
+
+
+help()
+
